ce53f30a6271405127ce4dc7dec1e3935cf94cb0
Marcus Twichel <marc.twichy@gmail.com>
2018-06-19 17:54:25 -0600

    Changed api services

--- lib/main.dart
+++ lib/main.dart
@@ -33,14 +33,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   FirebaseVisionTextDetector detector;
-  File _image;
-  List<VisionText> currentLabels;
+  List currentLabels;
   List ingredients;
 
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     detector = FirebaseVisionTextDetector.instance;
     currentLabels = [];
@@ -56,107 +54,62 @@ class _MyHomePageState extends State<MyHomePage> {
         title: new Text(widget.title),
       ),
       body: new Center(
-
         child: new Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new RaisedButton(
-              child: new Text("Get Image"),
-              onPressed:() async{
-                var image = await ImagePicker.pickImage(source: ImageSource.camera);
-                var textList = await detector.detectFromPath(image?.path);
-                var dataString = list2String(list: textList);
-                var result = await http.post(
-                  'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/parseIngredients?includeNutrition=true',
-                  headers: {'X-Mashape-Key': '7lqJ5Hl4RJmshs4bsOyrsroUiH2cp1pwFfjjsnU7UssW1DWdF3'},
-                  body: {'ingredientList': dataString, 'servings': "1"},
-                );
-                List resultJson = json.decode(result.body);
-                setState(() {
-                  ingredients = resultJson;
-                });
-              },
+            new Expanded(
+                child: currentLabels.length == 0
+                    ? new Text("no text")
+                    : new ListView.builder(
+                  itemBuilder: (BuildContext context, int index) =>
+                  new Column(
+                    children: <Widget>[
+                      new Text(currentLabels[index].text),
+                    ],
+                  ),
+                  itemCount: currentLabels.length,)
             ),
-            new Expanded(child: ingredients.length == 0
-                ? new Text("no text")
-                : new ListView.builder(
-              itemBuilder: (BuildContext context, int index) =>
-              new Column(
-                children: <Widget>[
-                  new Text(ingredients[index]["name"]),
-                  new Text(ingredients
-                  [index]
-                  ["nutrition"]
-//                  ["nutrients"]
-//                  [0]
-//                  ["title"]
-                  )
-                ],
-              ),
-              itemCount: ingredients.length,)
+            new Expanded(
+                child: ingredients.length == 0
+                    ? new Text("no text")
+                    : new ListView.builder(
+                  itemBuilder: (BuildContext context, int index) =>
+                  new Column(
+                    children: <Widget>[
+                      new Text(ingredients[index].toString()),
+                    ],
+                  ),
+                  itemCount: ingredients.length,)
             ),
           ],
         ),
       ),
       floatingActionButton: new FloatingActionButton(
-        onPressed: null,
+        onPressed: analyzePicture,
         tooltip: 'Increment',
         child: new Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-  String list2String({List<VisionText> list}) {
-    var ans = "";
-    list.forEach((item) {
-      ans += "${item.text}\n";
+  void analyzePicture() async{
+    var resultList = [];
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    var textList = await detector.detectFromPath(image?.path);
+    setState(() {
+      currentLabels = textList;
     });
-    return ans;
-  }
+    await Future.forEach(textList, (text) async{
+      var inputString = text.text;
+      var result = await http.get(
+        'https://api.edamam.com/api/nutrition-data?app_id=be779c43&app_key=42d7a3fee3bbda47cbbaf4db31875046&ingr=$inputString',);
+      var resultJson = await json.decode(result.body);
+      resultList.add(resultJson["totalNutrients"]["FAT"]["quantity"]);
+    });
 
-}
-
-class Ingredient {
-  final String name;
-  final num amount;
-  final String unit;
-  final Nutrition nutrition;
-
-  Ingredient({this.name, this.amount, this.unit, this.nutrition,});
-
-  factory Ingredient.fromJson(Map<String, dynamic> json) {
-    return new Ingredient(
-      name: json['name'],
-      amount: json['amount'],
-      unit: json['unit'],
-      nutrition: json['nutrition'],
-    );
-  }
-}
-
-class Nutrition {
-  final List<Nutrient> nutrients;
-
-  Nutrition({this.nutrients});
-
-  factory Nutrition.fromJson(Map<String, dynamic> json) {
-    return new Nutrition(
-      nutrients: json['nutrients'],
-    );
-  }
-}
-
-class Nutrient {
-  final String title;
-  final double amount;
-
-  Nutrient({this.title, this.amount});
-
-  factory Nutrient.fromJson(Map<String, dynamic> json) {
-    return new Nutrient(
-      title: json['title'],
-      amount: json['amount'],
-    );
+    setState(() {
+      ingredients = resultList;
+    });
   }
 }

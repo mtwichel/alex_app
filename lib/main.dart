3e4a4b0405143cf63e4d41110bf753f432de21f7
Marcus Twichel <marc.twichy@gmail.com>
2018-06-19 22:22:41 -0600

    Added ingredient view

--- lib/main.dart
+++ lib/main.dart
@@ -59,25 +59,15 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Expanded(
-                child: currentLabels.length == 0
-                    ? new Text("no text")
-                    : new ListView.builder(
-                  itemBuilder: (BuildContext context, int index) =>
-                  new Column(
-                    children: <Widget>[
-                      new Text(currentLabels[index].text),
-                    ],
-                  ),
-                  itemCount: currentLabels.length,)
-            ),
-            new Expanded(
                 child: ingredients.length == 0
                     ? new Text("no text")
                     : new ListView.builder(
                   itemBuilder: (BuildContext context, int index) =>
                   new Column(
                     children: <Widget>[
-                      new Text(ingredients[index].toString()),
+                      new IngredientView(
+                        ingredient: ingredients[index],
+                      ),
                     ],
                   ),
                   itemCount: ingredients.length,)
@@ -89,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: analyzePicture,
         tooltip: 'Increment',
         child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 
@@ -97,6 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
     var resultList = [];
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
     var textList = await detector.detectFromPath(image?.path);
+    print("Image Processed");
     setState(() {
       currentLabels = textList;
     });
@@ -105,11 +96,122 @@ class _MyHomePageState extends State<MyHomePage> {
       var result = await http.get(
         'https://api.edamam.com/api/nutrition-data?app_id=be779c43&app_key=42d7a3fee3bbda47cbbaf4db31875046&ingr=$inputString',);
       var resultJson = await json.decode(result.body);
-      resultList.add(resultJson["totalNutrients"]["FAT"]["quantity"]);
+      if(resultJson["calories"] != 0 && resultJson["totalWeight"] != 0.0){
+        print(text.text);
+        resultList.add(new Ingredient(
+          title: text.text,
+          calories: resultJson["calories"].toString(),
+          fat: getNutrient(resultJson, "FAT"),
+          carbs: getNutrient(resultJson, "CHOCDF"),
+          sugars: getNutrient(resultJson, "SUGAR"),
+          protein: getNutrient(resultJson, "PROCNT"),
+          sodium: getNutrient(resultJson, "NA"),
+          fiber: getNutrient(resultJson, "FIBTG"),
+        ));
+      }
     });
 
     setState(() {
       ingredients = resultList;
     });
   }
+
+  getNutrient(var json, var type){
+    if(json["totalNutrients"][type] == null){
+      return null;
+    }else{
+      return json["totalNutrients"][type]["quantity"].toString() + json["totalNutrients"][type]["unit"];
+    }
+  }
+}
+
+class IngredientView extends StatelessWidget{
+  IngredientView({this.ingredient});
+
+  var ingredient;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        new Text(ingredient.title.toString()),
+        new Center(
+          child: new Table(
+            columnWidths: {
+              0: new FractionColumnWidth(0.3),
+              1: new FractionColumnWidth(0.3),
+            },
+            children: [
+              TableRow(
+                  children: [
+                    new Text("Calories"),
+                    new Text(ingredient.calories.toString()),
+                  ]
+              ),
+              TableRow(
+                  children: [
+                    new Text("Fat"),
+                    new Text(ingredient.calories.toString()),
+                  ]
+              ),
+              TableRow(
+                  children: [
+                    new Text("Sugar"),
+                    new Text(ingredient.sugars.toString()),
+                  ]
+              ),
+              TableRow(
+                  children: [
+                    new Text("Protein"),
+                    new Text(ingredient.protein.toString()),
+                  ]
+              ),
+              TableRow(
+                  children: [
+                    new Text("Carbs"),
+                    new Text(ingredient.carbs.toString()),
+                  ]
+              ),
+              TableRow(
+                  children: [
+                    new Text("Sodium"),
+                    new Text(ingredient.sodium.toString()),
+                  ]
+              ),
+              TableRow(
+                  children: [
+                    new Text("Fiber"),
+                    new Text(ingredient.fiber.toString()),
+                  ]
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+}
+
+class Ingredient{
+  var title;
+  var calories;
+  var fat;
+  var carbs;
+  var sugars;
+  var protein;
+  var sodium;
+  var fiber;
+
+  Ingredient({
+    this.calories,
+    this.fat,
+    this.carbs,
+    this.sugars,
+    this.protein,
+    this.sodium,
+    this.fiber,
+    this.title,
+    });
+
 }
